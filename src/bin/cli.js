a039347d832961caa76df08f60c01c34b979feea
Johannes Maas <y0hy0h@gmx.net>
2020-09-27 16:47:08 +0200

    Add tsconfig, move code into src/, .gitignore dist/

--- src/bin/cli.js
+++ src/bin/cli.js
@@ -1,10 +1,10 @@
 #!/usr/bin/env node
 
-var findAllDependencies = require('../index.js').findAllDependencies;
+var findAllDependencies = require("../../index.js").findAllDependencies;
 
 var entry = process.argv[2];
 
 findAllDependencies(entry)
-  .then(function(dependencies) {
+  .then(function (dependencies) {
     console.log(dependencies);
   });
